9e30758727a813ce78c1e16f48623efa9eeb6346
Des Hartman <des@deshartman.com>
2024-04-18 13:39:18 +1000

    Added active CallSId as Security

--- Server/functions/sync/getSyncToken.js
+++ Server/functions/sync/getSyncToken.js
@@ -1,12 +1,15 @@
 /**
- * @param {string} identity - The identity of the user
+ * @param {string} identity - The identity of the callSid
  * 
  * Returns Sync Token used in the PayClient.
  */
-import { AccessToken } from 'twilio';
-var SyncGrant = AccessToken.SyncGrant;
+// import { AccessToken } from 'twilio';
+// var SyncGrant = AccessToken.SyncGrant;
 
 exports.handler = async (context, event, callback) => {
+
+    const AccessToken = require('twilio').jwt.AccessToken;
+    const SyncGrant = AccessToken.SyncGrant;
 
     // Add CORS handling headers
     const twilioResponse = new Twilio.Response();
@@ -14,6 +17,18 @@ exports.handler = async (context, event, callback) => {
     twilioResponse.appendHeader("Access-Control-Allow-Origin", "*");
     twilioResponse.appendHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
     twilioResponse.appendHeader("Content-Type", "application/json");
+
+    // Get a reference to the Twilio REST helper library
+    const twilioClient = context.getTwilioClient();
+
+    console.log(`Server getSyncToken: ${event.callSid}`);
+
+
+    // NOTE: Simple Security measure that will need to be strengthened. Check if there is a call in progress for this callSid.
+    const callResource = await twilioClient.calls(event.callSid).fetch();
+    if (callResource.status !== 'in-progress') {
+        return callback(`getSyncToken error: Call not in progress for ${event.callSid}`);
+    }
 
     // Create a "grant" identifying the Sync service instance for this app.
     var syncGrant = new SyncGrant({
@@ -29,7 +44,7 @@ exports.handler = async (context, event, callback) => {
     );
     // Add the Token specific optionsm
     token.addGrant(syncGrant);
-    token.identity = event.identity;
+    token.identity = event.callSid;
 
     if (token) {
         // Serialize the token to a JWT string and include it in a JSON response
